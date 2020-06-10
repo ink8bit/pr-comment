e9e541ca7450b4e4c14dd8a2adf1fba7aff248da
ink8bit <ink8bit@users.noreply.github.com>
2020-06-10 21:50:20 +0300

    Add clap crate
    See more info on crates.io: https://crates.io/crates/clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use clap::App;
+
 fn main() {
-    println!("Hello, world!");
+    App::new("myapp")
+        .version("0.0.1")
+        .about("Creates PR comment")
+        .author("ink8bit")
+        .get_matches();
 }
