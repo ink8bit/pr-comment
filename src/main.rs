12cfe33e6736124fa0a7366512731cb1ce1f8114
ink8bit <ink8bit@users.noreply.github.com>
2020-11-28 17:42:41 +0300

    Add arguments

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,37 @@
-use clap::App;
+use clap::{App, Arg};
 
 fn main() {
-    App::new("comment")
+    let app = App::new("comment")
         .version("0.0.1")
         .about("Creates PR comment")
         .author("ink8bit")
-        .get_matches();
+        .arg(
+            Arg::new("id")
+                .short('i')
+                .long("id")
+                .value_name("int")
+                .about("Sets Task ID value")
+                .required(true)
+                .takes_value(true),
+        )
+        .arg(
+            Arg::new("link")
+                .short('l')
+                .long("link")
+                .value_name("string")
+                .about("Sets PR links values, use comma for multiple values")
+                .required(true)
+                .takes_value(true),
+        )
+        .arg(
+            Arg::new("reviewer")
+                .short('r')
+                .long("reviewer")
+                .value_name("string")
+                .about("Sets a reviewer or reviewers, use comma for multiple values")
+                .takes_value(true),
+        );
+
+    let matches = app.get_matches();
+    println!("{:?}", matches);
 }
