89dcce0a64ede5c51f5941e57c1ae9e2a4a32b21
ink8bit <ink8bit@users.noreply.github.com>
2020-11-28 20:54:12 +0300

    Parse config file using serde

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,25 @@
 use clap::{App, Arg};
 use dirs;
+use serde::Deserialize;
+use std::collections::HashMap;
 use std::fs;
 
 const CONFIG_FILE: &str = ".commentrc.json";
+
+#[derive(Deserialize, Debug)]
+struct Config {
+    #[serde(rename(deserialize = "defaultReviewer"))]
+    default_reviewer: String,
+    #[serde(rename(deserialize = "templatePath"))]
+    template_path: String,
+    links: HashMap<String, LinkInfo>,
+}
+#[derive(Deserialize, Debug)]
+struct LinkInfo {
+    abbreviation: String,
+    description: String,
+    url: String,
+}
 
 fn main() {
     let app = App::new("comment")
@@ -46,4 +63,7 @@ fn main() {
 
     let config_data = fs::read_to_string(config_path).expect("Unable to read config file");
     println!("{}", config_data);
+
+    let deserialized: Config = serde_json::from_str(&config_data).unwrap();
+    println!("deserialized = {:?}", deserialized);
 }
