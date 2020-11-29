92115af8ca5df93cb383e3c30ec1d8f6a6b4bcb1
ink8bit <ink8bit@users.noreply.github.com>
2020-11-29 21:49:18 +0300

    Change app structure

--- src/comment.rs
+++ src/comment.rs
@@ -1,3 +1,6 @@
+use std::{collections::HashMap, error::Error};
+
+use super::config;
 pub struct Comment {
     pub id: String,
     pub reviewers: String,
@@ -24,4 +27,41 @@ _TODO:_ how to test changes you've made
 ",
         c.id, c.links, c.reviewers,
     )
+}
+
+pub fn reviewers(r_flag_value: &str, default_reviewer: String) -> Result<String, Box<dyn Error>> {
+    if r_flag_value.is_empty() && default_reviewer.is_empty() {
+        panic!("you haven't provided any reviewer.");
+    }
+
+    let mut rs = String::new();
+    if r_flag_value.is_empty() {
+        rs.push_str(&format!("@{}\n", default_reviewer));
+    }
+
+    let revs: Vec<&str> = r_flag_value.split(",").collect();
+    if revs.len() > 0 && !revs[0].is_empty() {
+        for rev in revs {
+            rs.push_str(&format!("@{}\n", rev));
+        }
+    }
+
+    Ok(rs)
+}
+
+pub fn links(l_flag_value: &str, config_links: HashMap<String, config::LinkInfo>) -> String {
+    let links: Vec<&str> = l_flag_value.split(",").collect();
+    let mut s = String::new();
+
+    for link in links {
+        let link_parts: Vec<&str> = link.split("/").collect();
+        let repo_abbrev = link_parts[0];
+        let pr_id = link_parts[1];
+        if config_links.contains_key(repo_abbrev) {
+            let val = config_links.get(repo_abbrev).unwrap();
+            s.push_str(&format!("- [{}]({}/{})\n", val.description, val.url, pr_id));
+        }
+    }
+
+    s
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use std::{collections::HashMap, error::Error};
-
 use clap::{App, Arg};
 use dirs;
 
@@ -7,7 +5,6 @@ mod comment;
 mod config;
 
 use comment::Comment;
-use config::{LinkInfo, CONFIG_FILE};
 
 fn main() {
     let app = App::new("comment")
@@ -42,7 +39,7 @@ fn main() {
         );
 
     let home_path = dirs::home_dir().expect("can't get $HOME dir path");
-    let config_file = config::path(home_path, CONFIG_FILE);
+    let config_file = config::path(home_path, config::FILE);
     let config = config::parse(config_file).expect("can't parse config file");
 
     let matches = app.get_matches();
@@ -50,8 +47,8 @@ fn main() {
     let l = matches.value_of("link").unwrap();
     let r = matches.value_of("reviewer").unwrap_or("");
     let dr = config.default_reviewer;
-    let rs = reviewers(r, dr).expect("can't create a list of reviewers.");
-    let ls = links(l, config.links);
+    let rs = comment::reviewers(r, dr).expect("can't create a list of reviewers.");
+    let ls = comment::links(l, config.links);
 
     let c = comment::create(Comment {
         id: id.to_string(),
@@ -60,41 +57,4 @@ fn main() {
     });
 
     println!("{}", c);
-}
-
-fn reviewers(r_flag_value: &str, default_reviewer: String) -> Result<String, Box<dyn Error>> {
-    if r_flag_value.is_empty() && default_reviewer.is_empty() {
-        panic!("you haven't provided any reviewer.");
-    }
-
-    let mut rs = String::new();
-    if r_flag_value.is_empty() {
-        rs.push_str(&format!("@{}\n", default_reviewer));
-    }
-
-    let revs: Vec<&str> = r_flag_value.split(",").collect();
-    if revs.len() > 0 && !revs[0].is_empty() {
-        for rev in revs {
-            rs.push_str(&format!("@{}\n", rev));
-        }
-    }
-
-    Ok(rs)
-}
-
-fn links(l_flag_value: &str, config_links: HashMap<String, LinkInfo>) -> String {
-    let links: Vec<&str> = l_flag_value.split(",").collect();
-    let mut s = String::new();
-
-    for link in links {
-        let link_parts: Vec<&str> = link.split("/").collect();
-        let repo_abbrev = link_parts[0];
-        let pr_id = link_parts[1];
-        if config_links.contains_key(repo_abbrev) {
-            let val = config_links.get(repo_abbrev).unwrap();
-            s.push_str(&format!("- [{}]({}/{})\n", val.description, val.url, pr_id));
-        }
-    }
-
-    s
 }
