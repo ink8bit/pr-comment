fc9b182b75832b4f0806e56d4a89bc314d5f58cf
ink8bit <ink8bit@users.noreply.github.com>
2020-11-29 21:57:19 +0300

    Add cli module

---
+++ src/cli.rs
@@ -0,0 +1,35 @@
+use clap::{App, Arg, ArgMatches};
+
+pub fn args() -> ArgMatches {
+    App::new("comment")
+        .version("0.0.1")
+        .about("comment is a CLI app which creates a formatted comment for your pull requests.")
+        .author("ink8bit")
+        .arg(
+            Arg::new("id")
+                .short('i')
+                .long("id")
+                .value_name("int")
+                .about("Sets Task ID value")
+                .required(true)
+                .takes_value(true),
+        )
+        .arg(
+            Arg::new("link")
+                .short('l')
+                .long("link")
+                .value_name("string")
+                .about("Sets PR links values, use comma for multiple values")
+                .required(true)
+                .takes_value(true),
+        )
+        .arg(
+            Arg::new("reviewer")
+                .short('r')
+                .long("reviewer")
+                .value_name("string")
+                .about("Sets a reviewer or reviewers, use comma for multiple values")
+                .takes_value(true),
+        )
+        .get_matches()
+}
