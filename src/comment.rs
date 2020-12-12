538084fe1c35c8c4dc0cb1e8ac03750451b9965d
ink8bit <ink8bit@users.noreply.github.com>
2020-12-12 16:15:04 +0300

    Add tests to comment module

--- src/comment.rs
+++ src/comment.rs
@@ -1,4 +1,6 @@
 use super::config::Config;
+
+#[derive(Debug)]
 pub struct Comment {
     pub id: String,
     pub reviewers: String,
@@ -8,11 +10,23 @@ pub struct Comment {
 }
 
 impl Comment {
-    pub fn new(self) -> Result<String, String> {
+    pub fn new(self) -> Result<Self, String> {
         let branch_name = Comment::branch_name(&self);
         let reviewers = Comment::reviewers(&self)?;
         let links = Comment::links(&self);
 
+        let comment = Self {
+            id: branch_name,
+            reviewers,
+            links,
+            config: self.config,
+            is_bug: self.is_bug,
+        };
+
+        Ok(comment)
+    }
+
+    pub fn print(self) -> String {
         let comment = format!(
             "
 **PULL REQUEST**
@@ -28,12 +42,12 @@ _TODO:_ what you've changed
 **TESTING**
 _TODO:_ how to test changes you've made
     ",
-            branch = branch_name,
-            links = links,
-            review = reviewers,
+            branch = self.id,
+            links = self.links,
+            review = self.reviewers,
         );
 
-        Ok(comment)
+        comment.trim().to_string()
     }
 
     fn branch_name(&self) -> String {
@@ -78,5 +92,126 @@ _TODO:_ how to test changes you've made
         }
 
         s
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+
+    use super::{Comment, Config};
+
+    #[test]
+    fn should_create_comment() {
+        let links = HashMap::new();
+        let config = Config {
+            default_reviewer: "".to_string(),
+            links,
+        };
+        let c = Comment {
+            id: "123".to_string(),
+            links: "b/1".to_string(),
+            reviewers: "example_reviewer".to_string(),
+            is_bug: false,
+            config,
+        };
+        let c = Comment::new(c).unwrap();
+        assert_eq!(c.id, "feature/123");
+        assert_eq!(c.links, "".to_string());
+        assert_eq!(c.reviewers.trim(), "@example_reviewer");
+    }
+
+    #[test]
+    fn should_create_bugfix() {
+        let links = HashMap::new();
+        let config = Config {
+            default_reviewer: "default_reviewer".to_string(),
+            links,
+        };
+        let c = Comment {
+            id: "123".to_string(),
+            links: "b/1".to_string(),
+            reviewers: "".to_string(),
+            is_bug: true,
+            config,
+        };
+        let c = Comment::new(c).unwrap();
+        assert_eq!(c.id, "bugfix/123");
+    }
+
+    #[test]
+    fn should_use_default_reviewer() {
+        let links = HashMap::new();
+        let config = Config {
+            default_reviewer: "default_reviewer".to_string(),
+            links,
+        };
+        let c = Comment {
+            id: "123".to_string(),
+            links: "b/1".to_string(),
+            reviewers: "".to_string(),
+            is_bug: false,
+            config,
+        };
+        let c = Comment::new(c).unwrap();
+        assert_eq!(c.reviewers.trim(), "@default_reviewer");
+    }
+
+    #[test]
+    fn should_use_reviewers() {
+        let links = HashMap::new();
+        let config = Config {
+            default_reviewer: "default_reviewer".to_string(),
+            links,
+        };
+        let c = Comment {
+            id: "123".to_string(),
+            links: "b/1".to_string(),
+            reviewers: "example_reviewer_one,example_reviewer_two".to_string(),
+            is_bug: false,
+            config,
+        };
+        let c = Comment::new(c).unwrap();
+        assert_eq!(
+            c.reviewers,
+            "@example_reviewer_one\n@example_reviewer_two\n"
+        );
+    }
+
+    #[test]
+    #[should_panic(expected = "You haven\\\'t provided any reviewer.\"")]
+    fn should_panic_when_no_reviewers() {
+        let links = HashMap::new();
+        let config = Config {
+            default_reviewer: "".to_string(),
+            links,
+        };
+        let c = Comment {
+            id: "123".to_string(),
+            links: "b/1".to_string(),
+            reviewers: "".to_string(),
+            is_bug: false,
+            config,
+        };
+        let c = Comment::new(c).unwrap();
+        assert_eq!(c.reviewers, "");
+    }
+
+    #[test]
+    fn should_print_comment() {
+        let links = HashMap::new();
+        let config = Config {
+            default_reviewer: "default_reviewer".to_string(),
+            links,
+        };
+        let c = Comment {
+            id: "123".to_string(),
+            links: "b/1".to_string(),
+            reviewers: "".to_string(),
+            is_bug: false,
+            config,
+        };
+        let c = Comment::new(c).unwrap();
+        assert_eq!(c.print(), "**PULL REQUEST**\nfeature/123\n\n**LINKS**\n\n**REVIEW**\n@default_reviewer\n\n**CHANGES**\n_TODO:_ what you\'ve changed\n\n**TESTING**\n_TODO:_ how to test changes you\'ve made");
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,13 @@ use config::Config;
 
 fn main() {
     let args = cli::args();
-    let config = Config::new().expect("Couldn't parse config file.");
     let id = args.value_of("id").unwrap();
     let l = args.value_of("link").unwrap();
     let r = args.value_of("reviewer").unwrap_or("");
     let is_bug = args.is_present("bug");
     let need_copy = args.is_present("copy");
+
+    let config = Config::new().expect("Couldn't parse config file.");
 
     let comment = Comment {
         id: id.to_string(),
@@ -25,11 +26,11 @@ fn main() {
     };
 
     let output = Comment::new(comment).expect("Could not create comment.");
-
-    println!("{}", output);
+    let printed = output.print();
+    println!("{}", &printed);
 
     if need_copy {
         let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
-        ctx.set_contents(output).unwrap();
+        ctx.set_contents(printed).unwrap();
     }
 }
