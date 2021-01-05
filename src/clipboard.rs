8835e08c74a7ffdc629b89f717709804b6c65afd
ink8bit <ink8bit@users.noreply.github.com>
2021-01-05 22:03:22 +0300

    Add tests to clipboard module

--- src/clipboard.rs
+++ src/clipboard.rs
@@ -4,10 +4,38 @@ use clipboard::{ClipboardContext, ClipboardProvider};
 
 /// Copies comment to system clipboard
 pub fn copy<'a>(need_copy: bool, comment: String) -> Result<&'a str, Box<dyn Error>> {
-    if need_copy {
-        let mut ctx: ClipboardContext = ClipboardProvider::new()?;
-        ctx.set_contents(comment).unwrap();
+    if !need_copy {
+        return Ok("");
+    }
+    let mut ctx: ClipboardContext = ClipboardProvider::new()?;
+    ctx.set_contents(comment).unwrap();
+    Ok("\nYour comment was successfully copied to your clipboard.")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::copy;
+    use clipboard::{ClipboardContext, ClipboardProvider};
+
+    #[test]
+    fn should_copy_string_value_to_clipboard() {
+        let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
+        let fake_text = "fake text";
+        let msg = copy(true, fake_text.to_string()).unwrap();
+
+        assert_eq!(
+            msg,
+            "\nYour comment was successfully copied to your clipboard."
+        );
+        assert_eq!(ctx.get_contents().unwrap(), fake_text);
     }
 
-    Ok("\nYour comment was successfully copied to your clipboard.")
+    #[test]
+    fn should_copy_if_flag_provided() {
+        let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
+        let fake_text = "fake text";
+        let _msg = copy(false, fake_text.to_string()).unwrap();
+
+        assert_ne!(ctx.get_contents().unwrap_or_default(), fake_text);
+    }
 }
